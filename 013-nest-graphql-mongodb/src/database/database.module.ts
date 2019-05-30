239022e7d2acb2e1b1f4fb63e17b38462118b530
Christiano de Andrade Bovo <chrisabovo@gmail.com>
2019-05-30 19:06:26 +0000

    chore: removendo.

--- 013-nest-graphql-mongodb/src/database/database.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-
-@Module({
-  imports: [
-    MongooseModule.forRoot('mongodb://spvm026elcs.elucid.int/nest-graphql-mongodb'),
-  ]
-})
-export class DatabaseModule {}
